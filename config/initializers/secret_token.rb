2f37f8773e1482018995401c766ee8d5983ef46f
Ana Martinez <anacecilia.martinez@sv.cmu.edu>
2011-10-11 11:51:29 -0700

    Skeleton of the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JavascriptTest::Application.config.secret_token = 'c33ad9a4524f41c7b16a398341d803edb52bb99ffa59b15f70fe064a26115971c71289bea1d55392e86b19a14152f7b6135b5b885abfc2186a09750ffc7bd687'
